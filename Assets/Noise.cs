d24d9b0b617e6028042011982265de3875df685f
Christan Shane Plaza <christan.shane.plaza@sun-asterisk.com>
2021-06-09 17:51:28 +0800

    Noise Map Generation

--- Assets/Noise.cs
+++ Assets/Noise.cs
@@ -4,28 +4,29 @@ using UnityEngine;
 
 public static class Noise
 {
-    // Noisemap Generator
-    public static float[,] GenerateNoiseMap(int mapWidth, int mapHeight, float scale)
-    {
-        float[,] noiseMap = new float[mapWidth, mapHeight];
 
-        if (scale <= 0)
-        {
-            scale = 0.0001f;
-        }
+	public static float[,] GenerateNoiseMap(int mapWidth, int mapHeight, float scale)
+	{
+		float[,] noiseMap = new float[mapWidth, mapHeight];
 
-        for (int y = 0; y < mapHeight; y++)
-        {
-            for (int x = 0; x < mapWidth; x++)
-            {
-                float sampleX = x / scale;
-                float sampleY = y / scale;
+		if (scale <= 0)
+		{
+			scale = 0.0001f;
+		}
 
-                float perlinValue = Mathf.PerlinNoise(sampleX, sampleY);
-                noiseMap[x, y] = perlinValue;
-            }
-        }
+		for (int y = 0; y < mapHeight; y++)
+		{
+			for (int x = 0; x < mapWidth; x++)
+			{
+				float sampleX = x / scale;
+				float sampleY = y / scale;
 
-        return noiseMap;
-    }
+				float perlinValue = Mathf.PerlinNoise(sampleX, sampleY);
+				noiseMap[x, y] = perlinValue;
+			}
+		}
+
+		return noiseMap;
+	}
+
 }
